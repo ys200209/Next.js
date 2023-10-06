e165d9d9bdd13268166cfa692bfa39c0bb8b767e
이 세영 <lys200209@naver.com>
2023-10-06 09:53:51 +0900

    feat: Apply OAuth2.0 with Github

--- src/components/Grids/Grids.tsx
+++ src/components/Grids/Grids.tsx
@@ -1,47 +1,43 @@
 "use client"
 
 import {Button, Container, Grid, rem, SimpleGrid, Skeleton, useMantineTheme} from '@mantine/core';
-import {useEffect, useState} from "react";
-import ShowChart from "@/components/ShowChart/ShowChart";
-import Users from "@/components/Users/Users";
+import {lazy, ReactNode, Suspense, useEffect, useState} from "react";
+import { Loader } from '@mantine/core';
 
 const PRIMARY_COL_HEIGHT = rem(300);
 
 export default function Grids() {
-    const [isLoading, setIsLoading] = useState(true); // 로딩 상태 관리
-    const [isUsersClicked, setIsUsersClicked] = useState(false);
-    const [isChartClicked, setIsChartClicked] = useState(false);
-
-    const theme = useMantineTheme();
-    const SECONDARY_COL_HEIGHT = `calc(${PRIMARY_COL_HEIGHT} / 2 - ${theme.spacing.md} / 2)`;
-    const TERTIARY_COL_HEIGHT = `calc(${PRIMARY_COL_HEIGHT} / 3 - ${theme.spacing.md} / 1)`;
-
     const diagramStyle = {
         width: '100%',
         height: '500px',
         backgroundColor: '#DAE4E4',
     };
 
+    const [isLoading, setIsLoading] = useState(true); // 로딩 상태 관리
+    const theme = useMantineTheme();
+    const SECONDARY_COL_HEIGHT = `calc(${PRIMARY_COL_HEIGHT} / 2 - ${theme.spacing.md} / 2)`;
+    const TERTIARY_COL_HEIGHT = `calc(${PRIMARY_COL_HEIGHT} / 3 - ${theme.spacing.md} / 1)`;
+
+    const [LazyComponent, setLazyComponent] = useState<ReactNode | null>(null);
+    const Users = lazy(() => import('@/components/Users/Users'));
+    const ShowChart = lazy(() => import('@/components/ShowChart/ShowChart'));
+    const JsonAnalysisChart = lazy(() => import('@/components/JsonAnalysisChart/JsonAnalysisChart'));
+
     useEffect(() => {
-        // 데이터 또는 컴포넌트 초기화 작업을 여기에서 수행
-
-        // 초기화 작업이 완료되면 로딩 상태를 해제
-        // setIsLoading(false);
-        /*const timer = setTimeout(() => {
-            setIsLoading(false);
-        }, 3000);*/
-
         setIsLoading(false);
     }, []);
 
+
     function clickUsers() {
-        setIsUsersClicked(true);
-        setIsChartClicked(false);
+        setLazyComponent(Users);
     }
 
-    function clickChart() {
-        setIsChartClicked(true);
-        setIsUsersClicked(false);
+    function clickMBTI() {
+        setLazyComponent(ShowChart);
+    }
+
+    function clickAnalysis() {
+        setLazyComponent(JsonAnalysisChart);
     }
 
     return (
@@ -56,6 +52,7 @@ export default function Grids() {
                             ) : (
                                 <Button
                                     fullWidth
+                                    variant="outline"
                                     radius="md"
                                     style={{minHeight: `${TERTIARY_COL_HEIGHT}`}}
                                     onClick={clickUsers}>
@@ -70,9 +67,10 @@ export default function Grids() {
                             ) : (
                                 <Button
                                     fullWidth
+                                    variant="outline"
                                     radius="md"
                                     style={{minHeight: `${TERTIARY_COL_HEIGHT}`}}
-                                    onClick={clickChart}>
+                                    onClick={clickMBTI}>
                                     차트 조회
                                 </Button>
                             )}
@@ -84,9 +82,11 @@ export default function Grids() {
                             ) : (
                                 <Button
                                     fullWidth
+                                    variant="outline"
                                     radius="md"
-                                    style={{minHeight: `${TERTIARY_COL_HEIGHT}`}}>
-                                    회원가입
+                                    style={{minHeight: `${TERTIARY_COL_HEIGHT}`}}
+                                    onClick={clickAnalysis}>
+                                    유저 분석
                                 </Button>
                             )}
                         </Grid.Col>
@@ -94,11 +94,10 @@ export default function Grids() {
                 </SimpleGrid>
             </Container>
 
-            {isChartClicked || isUsersClicked? (
-                isChartClicked ? <ShowChart /> : <Users />
-            ): <Skeleton style={diagramStyle}/>}
-            {/*{isUsersClicked ? <Button style={diagramStyle}>유저 누르셨어요??</Button> : <Skeleton style={diagramStyle}/>}*/}
-
+            {LazyComponent ? (
+                <Suspense fallback={<Loader/>}>
+                    <LazyComponent/>
+                </Suspense>) : <Skeleton style={diagramStyle}/>}
         </>
     );
 }
